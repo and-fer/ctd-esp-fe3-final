e8dee926f296da469ad1fc06f127791f43227827
Andres Ferrada <an.ferrada@gmail.com>
2024-12-06 23:26:42 -0300

    Corregir ruta de imagen del footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,10 +1,12 @@
+import DHLogo from '../../public/images/DH.png'
+
 const Footer = () => {
-   return (
-      <footer>
-         <p>Powered by</p>
-         <img src='../public/images/DH.png' alt='DH-logo' />
-      </footer>
-   )
+  return (
+    <footer>
+      <p>Powered by</p>
+      <img src={DHLogo} alt="DH-logo" />
+    </footer>
+  )
 }
 
 export default Footer
